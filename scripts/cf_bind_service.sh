64f573d8eb8c4c443d1d11b8a69bf650ad1d587f
Michael Goddard <mgoddard@gopivotal.com>
2016-10-12 09:28:23 -0400

    Better documentation in the 'cf bind-service' script

--- scripts/cf_bind_service.sh
+++ scripts/cf_bind_service.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+app_name="gcp-next-demo"
+
 #service="gcp-ml"
-service="gcp-ml-cups" # User-provided service (interim solution)
+#service="gcp-ml-cups"
 
-# roles/viewer: Permissions for read-only actions that preserve state.
+# This should be sufficient to enable ML and BigQuery, along with GCS (Storage)
+service="gcp-storage"
+
+# Additional configuration parameters: varies depending on which service
 # Ref. https://cloud.google.com/iam/docs/understanding-roles
+#
+# ML, BigQuery:  -c '{"role": "viewer"}'
+# Storage (GCS): -c '{"role": "editor"}'
 
-cf bind-service gcp-next-demo $service -c '{"role": "viewer"}'
+cf bind-service $app_name $service -c '{"role": "editor"}'
 
