a57a4d8f17ada8e0126111c974f20594bf6728e4
Michael Goddard <mgoddard@gopivotal.com>
2016-11-02 11:00:18 -0400

    Adding images to the 'resources' dir of the tests, so we can run our tests again

--- src/main/java/io/pivotal/CredentialManager.java
+++ src/main/java/io/pivotal/CredentialManager.java
@@ -33,113 +33,113 @@ import com.google.api.services.vision.v1.Vision;
 
 public class CredentialManager {
 
-	private static final String VCAP_KEY = "google-storage"; // "google-ml-apis", "user-provided"
-	private String privateKeyData;
-	private String projectId;
+    private static final String VCAP_KEY = "google-storage"; // "google-ml-apis", "user-provided"
+    private String privateKeyData;
+    private String projectId;
 
-	public CredentialManager() {
-		String env = System.getenv("VCAP_SERVICES");
-		//System.out.println(env);
-		JSONObject json = new JSONObject(env);
-		JSONArray root = json.getJSONArray(VCAP_KEY);
-		//System.out.println("root: " + root);
-		JSONObject obj0 = root.getJSONObject(0);
-		JSONObject cred = obj0.getJSONObject("credentials");
-		this.privateKeyData = cred.getString("PrivateKeyData");
-		String email = cred.getString("Email");
-		this.projectId = matchProjectId(email);
-		//System.out.println("PrivateKeyData: " + privateKeyData);
-		//System.out.println("project ID: " + projectId);
-	}
+    public CredentialManager() {
+        String env = System.getenv("VCAP_SERVICES");
+        //System.out.println(env);
+        JSONObject json = new JSONObject(env);
+        JSONArray root = json.getJSONArray(VCAP_KEY);
+        //System.out.println("root: " + root);
+        JSONObject obj0 = root.getJSONObject(0);
+        JSONObject cred = obj0.getJSONObject("credentials");
+        this.privateKeyData = cred.getString("PrivateKeyData");
+        String email = cred.getString("Email");
+        this.projectId = matchProjectId(email);
+        //System.out.println("PrivateKeyData: " + privateKeyData);
+        //System.out.println("project ID: " + projectId);
+    }
 
-	private String matchProjectId (String email) {
-		Pattern p = Pattern.compile("^[^@]+@([^.]+)\\..+$");
-		Matcher m = p.matcher(email);
-		String rv = "No match";
-		if (m.matches()) {
-			rv = m.group(1);
-		}
-		return rv;
-	}
+    private String matchProjectId(String email) {
+        Pattern p = Pattern.compile("^[^@]+@([^.]+)\\..+$");
+        Matcher m = p.matcher(email);
+        String rv = "No match";
+        if (m.matches()) {
+            rv = m.group(1);
+        }
+        return rv;
+    }
 
-	public String getPrivateKeyData () {
-		return this.privateKeyData;
-	}
+    public String getPrivateKeyData() {
+        return this.privateKeyData;
+    }
 
-	public String getProjectId () {
-		return this.projectId;
-	}
+    public String getProjectId() {
+        return this.projectId;
+    }
 
-	public  CloudNaturalLanguageAPI getNLPAPI() throws IOException, GeneralSecurityException {
-		HttpTransport trans = GoogleNetHttpTransport.newTrustedTransport();
-		JsonFactory jFactory = JacksonFactory.getDefaultInstance();
-		GoogleCredential cred = credential();
-		if (cred.createScopedRequired()) {
-			cred = cred.createScoped(CloudNaturalLanguageAPIScopes.all());
-		}
-		return new CloudNaturalLanguageAPI.Builder(trans, jFactory, cred).setApplicationName(APP_NAME).build();
-	}
+    public CloudNaturalLanguageAPI getNLPAPI() throws IOException, GeneralSecurityException {
+        HttpTransport trans = GoogleNetHttpTransport.newTrustedTransport();
+        JsonFactory jFactory = JacksonFactory.getDefaultInstance();
+        GoogleCredential cred = credential();
+        if (cred.createScopedRequired()) {
+            cred = cred.createScoped(CloudNaturalLanguageAPIScopes.all());
+        }
+        return new CloudNaturalLanguageAPI.Builder(trans, jFactory, cred).setApplicationName(APP_NAME).build();
+    }
 
-	public  Vision getVisionService() throws IOException, GeneralSecurityException {
-		String APPLICATION_NAME = "Landmark Finder";
+    public Vision getVisionService() throws IOException, GeneralSecurityException {
+        String APPLICATION_NAME = "Landmark Finder";
 
-		JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
-		GoogleCredential cred = credential();
-		if (cred.createScopedRequired()) {
-			cred = cred.createScoped(CloudNaturalLanguageAPIScopes.all());
-		}
+        JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+        GoogleCredential cred = credential();
+        if (cred.createScopedRequired()) {
+            cred = cred.createScoped(CloudNaturalLanguageAPIScopes.all());
+        }
 
-		return new Vision.Builder(GoogleNetHttpTransport.newTrustedTransport(), jsonFactory, cred)
-				.setApplicationName(APPLICATION_NAME)
-				.build();
-	}
+        return new Vision.Builder(GoogleNetHttpTransport.newTrustedTransport(), jsonFactory, cred)
+                .setApplicationName(APPLICATION_NAME)
+                .build();
+    }
 
-	public  Bigquery getBiqQueryClient() throws IOException {
-		// Create the credential
-		HttpTransport transport = new NetHttpTransport();
-		JsonFactory jsonFactory = new JacksonFactory();
-		GoogleCredential cred = credential();
-		// Depending on the environment that provides the default credentials (e.g. Compute Engine, App
-		// Engine), the credentials may require us to specify the scopes we need explicitly.
-		// Check for this case, and inject the Bigquery scope if required.
-		if (cred.createScopedRequired()) {
-			cred = cred.createScoped(CloudNaturalLanguageAPIScopes.all());
-		}
-		return new Bigquery.Builder(transport, jsonFactory, cred)
-				.setApplicationName("Bigquery Samples")
-				.build();
-	}
+    public Bigquery getBiqQueryClient() throws IOException {
+        // Create the credential
+        HttpTransport transport = new NetHttpTransport();
+        JsonFactory jsonFactory = new JacksonFactory();
+        GoogleCredential cred = credential();
+        // Depending on the environment that provides the default credentials (e.g. Compute Engine, App
+        // Engine), the credentials may require us to specify the scopes we need explicitly.
+        // Check for this case, and inject the Bigquery scope if required.
+        if (cred.createScopedRequired()) {
+            cred = cred.createScoped(CloudNaturalLanguageAPIScopes.all());
+        }
+        return new Bigquery.Builder(transport, jsonFactory, cred)
+                .setApplicationName("Bigquery Samples")
+                .build();
+    }
 
-	public Storage getStorageClient() throws IOException {
-            HttpTransport transport = new NetHttpTransport();
-            JsonFactory jsonFactory = new JacksonFactory();
-            GoogleCredential cred = credential();
-	        if (cred.createScopedRequired()) {
-	            cred = cred.createScoped(StorageScopes.all());
-	        }
-	        return new Storage.Builder(transport, jsonFactory, cred)
-	        .setApplicationName(APP_NAME).build();
-	}
+    public Storage getStorageClient() throws IOException {
+        HttpTransport transport = new NetHttpTransport();
+        JsonFactory jsonFactory = new JacksonFactory();
+        GoogleCredential cred = credential();
+        if (cred.createScopedRequired()) {
+            cred = cred.createScoped(StorageScopes.all());
+        }
+        return new Storage.Builder(transport, jsonFactory, cred)
+                .setApplicationName(APP_NAME).build();
+    }
 
-	private static GoogleCredential credential = null;
-	private static final String APP_NAME = "spring-nlp";
+    private static GoogleCredential credential = null;
+    private static final String APP_NAME = "spring-nlp";
 
-	private GoogleCredential credential() throws IOException {
-		if (credential == null) {
-			InputStream stream = new ByteArrayInputStream(Base64.getDecoder().decode(getPrivateKeyData()));
-			credential = GoogleCredential.fromStream(stream);
-		}
-		return credential;
-	}
+    private GoogleCredential credential() throws IOException {
+        if (credential == null) {
+            InputStream stream = new ByteArrayInputStream(Base64.getDecoder().decode(getPrivateKeyData()));
+            credential = GoogleCredential.fromStream(stream);
+        }
+        return credential;
+    }
 
-	@SuppressWarnings("unused")
-	private static void printClasspath() {
-		ClassLoader cl = ClassLoader.getSystemClassLoader();
-		URL[] urls = ((URLClassLoader) cl).getURLs();
-		for (URL url : urls) {
-			System.out.println(url.getFile());
-		}
-	}
+    @SuppressWarnings("unused")
+    private static void printClasspath() {
+        ClassLoader cl = ClassLoader.getSystemClassLoader();
+        URL[] urls = ((URLClassLoader) cl).getURLs();
+        for (URL url : urls) {
+            System.out.println(url.getFile());
+        }
+    }
 
 
 }
--- src/test/java/io/pivotal/GCPApplicationTests.java
+++ src/test/java/io/pivotal/GCPApplicationTests.java
@@ -11,49 +11,73 @@ import org.junit.Test;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.security.GeneralSecurityException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotEquals;
 
 public class GCPApplicationTests {
 
+    private Map<String, Path> fileToPath;
 
-    @Test
-    public void accessVisionApiTest () throws Exception {
-        VisionApiService vps = new VisionApiService();
-
-        byte[] array = Files.readAllBytes(new File("/Users/mross/Downloads/bridge.jpeg").toPath());
-
-        assertFalse(vps.requestLandmarkInfo(array).isEmpty());
-
-        array = Files.readAllBytes(new File("/Users/mross/Downloads/taj.jpeg").toPath());
-
-        assertFalse(vps.requestLandmarkInfo(array).isEmpty());
-
+    public GCPApplicationTests() {
+        fileToPath = new HashMap<>();
     }
 
+    // Provide access to files within src/test/resources/ directory
+    private Path getPath(String fileName) {
+        Path rv;
+        System.out.println("Getting path for " + fileName);
+        if (fileToPath.containsKey(fileName)) {
+            rv = fileToPath.get(fileName);
+        } else {
+            //ClassLoader classLoader = getClass().getClassLoader();
+            //File file = new File(classLoader.getResource(fileName).getFile()); // Better
+            //File file = new File(getClass().getResource(fileName).getFile()); // Worse
+            String path = this.getClass().getClassLoader().getResource(fileName).getPath();
+            if (path != null) {
+                System.out.println("Path: " + path);
+            } else {
+                System.out.println("Path is null!");
+            }
+            File file = new File(path);
+            rv = file.toPath();
+            fileToPath.put(fileName, rv);
+        }
+        return rv;
+    }
 
     @Test
-    public void accessLandmarkApiTest () throws Exception {
+    public void accessVisionApiTest() throws Exception {
         VisionApiService vps = new VisionApiService();
+        byte[] array = Files.readAllBytes(getPath("bridge.jpg"));
+        assertFalse(vps.requestLandmarkInfo(array).isEmpty());
+        array = Files.readAllBytes(getPath("taj.jpg"));
+        assertFalse(vps.requestLandmarkInfo(array).isEmpty());
+    }
 
-        byte[] array = Files.readAllBytes(new File("/Users/mross/Downloads/bridge.jpeg").toPath());
+    @Test
+    public void accessLandmarkApiTest() throws Exception {
+        VisionApiService vps = new VisionApiService();
+        
+        byte[] array = Files.readAllBytes(getPath("bridge.jpg"));
+        //assertFalse(vps.requestPhotoLabelInfo(array).isEmpty());
+        assertFalse(vps.identifyLandmark(array, 10).isEmpty());
 
-        assertFalse(vps.requestPhotoLabelInfo(array).isEmpty());
-
-        array = Files.readAllBytes(new File("/Users/mross/Downloads/taj.jpeg").toPath());
-
-        assertFalse(vps.requestPhotoLabelInfo(array).isEmpty());
-
+        array = Files.readAllBytes(getPath("taj.jpg"));
+        //assertFalse(vps.requestPhotoLabelInfo(array).isEmpty());
+        assertFalse(vps.identifyLandmark(array, 10).isEmpty());
     }
 
     @Test
     public void testExecuteBigQueryQuery() throws IOException {
         BigQueryApiService bqs = new BigQueryApiService("Taj Mahal");
 
-        java.util.List<TableRow> results =bqs.executeQuery();
+        List<TableRow> results = bqs.executeQuery();
         System.out.println("Iterating over returned results");
         System.out.println(results.size());
         for (TableRow row : results) {
@@ -62,63 +86,41 @@ public class GCPApplicationTests {
             }
             System.out.println();
         }
-
     }
 
+    /*
     @Test
     public void testExecuteBigQueryQueryWithLandmarkName() throws IOException, GeneralSecurityException {
         VisionApiService vps = new VisionApiService();
-
-        byte[] array =Files.readAllBytes(new File("/Users/mross/Downloads/taj.jpeg").toPath());
-//
-
+        byte[] array = Files.readAllBytes(getPath("taj.jpeg"));
         List<EntityAnnotation> landmarkInfoArray = vps.requestLandmarkInfo(array);
         assertFalse(landmarkInfoArray.isEmpty());
         EntityAnnotation landmarkResult = landmarkInfoArray.get(0);
         String landmarkName = landmarkResult.getDescription();
         System.out.println(landmarkName.trim());
-
         BigQueryApiService bqs = new BigQueryApiService(landmarkName);
-
-        java.util.List<TableRow> results =bqs.executeQuery();
-        assertNotEquals(0,results.size());
+        java.util.List<TableRow> results = bqs.executeQuery();
+        assertNotEquals(0, results.size());
         for (TableRow row : results) {
             for (TableCell field : row.getF()) {
                 System.out.printf("%-50s", field.getV());
             }
             System.out.println();
         }
-
-
-
-    array =Files.readAllBytes(new File("/Users/mross/Downloads/loc-flatiron1.jpg").toPath());
-
-
+        array = Files.readAllBytes(getPath("flatiron_building.jpg"));
         landmarkInfoArray = vps.requestLandmarkInfo(array);
         assertFalse(landmarkInfoArray.isEmpty());
         landmarkResult = landmarkInfoArray.get(0);
         landmarkName = landmarkResult.getDescription();
-
         System.out.println(landmarkName.trim());
-
-       bqs = new BigQueryApiService(landmarkName);
-
-         results =bqs.executeQuery();
-        assertNotEquals(0,results.size());
-
+        bqs = new BigQueryApiService(landmarkName);
+        results = bqs.executeQuery();
+        assertNotEquals(0, results.size());
         for (TableRow row : results) {
-        for (TableCell field : row.getF()) {
-            System.out.printf("%-50s", field.getV());
+            for (TableCell field : row.getF()) {
+                System.out.printf("%-50s", field.getV());
+            }
+            System.out.println();
         }
-        System.out.println();
-    }
-
-}
-
-
-
-
-
-
-
+    } */
 }
